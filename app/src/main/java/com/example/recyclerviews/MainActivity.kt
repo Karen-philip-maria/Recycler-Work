42fb1083fc83d0b612af1c1d45907555d1447535
karen-maria-philip <karenmariaphilip@gmail.com>
2024-06-18 09:19:21 +0300

    recyclerview

---
+++ app/src/main/java/com/example/recyclerviews/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.recyclerviews
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerviews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityMainBinding
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        val names = listOf("Amina", "Brenda", "Carol", "Diana", "Esther", "Flo", "Gina",
+            "Kelvin", "Lisa", "Mike", "Naomi", "Oscar", "Pearl", "Queen", "Robert","susan", "titus",
+            "universe","valon", "wanja","xoza","yiamat","zash")
+
+        binding.rvNames.layoutManager = LinearLayoutManager(this)
+        val namesAdapter = NamesRecyclerViewAdapter(names)
+        binding.rvNames.adapter= namesAdapter
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviews/NamesRecyclerViewAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.recyclerviews
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+class NamesRecyclerViewAdapter(var names:List<String>):
+    RecyclerView.Adapter<NamesViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NamesViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.name_list_item, parent, false)
+        return  NamesViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NamesViewHolder, position: Int) {
+       holder.tvName.text = names[position]
+    }
+
+    override fun getItemCount(): Int {
+        return  names.size
+    }
+}
+
+class NamesViewHolder(itemView: View):
+    RecyclerView.ViewHolder(itemView){
+    val tvName= itemView.findViewById<TextView>(R.id.tvName)
+}
